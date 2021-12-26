171b24f688370f71225501ea620b47f70b4d79bb
VladimirFerko <vladoferko3@gmail.com>
2021-12-27 00:26:00 +0100

    added feature for users

--- functions.py
+++ functions.py
@@ -2,10 +2,30 @@ from selenium import webdriver
 import time
 
 
+# func for gettin users input
+
+def get_user_input():
+    
+    print('hi, there are your options to choose from')
+    print('1 - scrape text from a blog\n2 - search something in google\n3 - compare prices on heureka')
+
+    while True:
+        try:
+            option = int(input("what u wanna do ? "))
+            if option <= 0 or option > 3:
+                continue
+            break
+        except ValueError:
+            print('Give me an int...')
+
+    return option
+
+
+
 # function for opening google chrome driver 
 
 def open_driver(DRIVER_PATH):
     driver = webdriver.Chrome(DRIVER_PATH)
-    driver.get("https://www.facebook.com/")
+    driver.get("https://www.google.com/")
     time.sleep(4)
     return driver
--- main.py
+++ main.py
@@ -1,13 +1,49 @@
 from selenium import webdriver
 import functions
+import psycopg2
+import os
+import time 
 
 DRIVER_PATH = '/home/vladimir/projects/webScrp/chromedriver'
+LINKS = ('https://dennikn.sk/blog/','https://blog.sme.sk/','https://www.wikipedia.org/') # links for blogs
+        
 
 if __name__ == '__main__':
-    driver = functions.open_driver(DRIVER_PATH)
+
+    user_opt = functions.get_user_input()
+
+    # clearing the terminal
+
+    os.system('cls||clear')
+
+    # option 1, also choosing a link
+
+    if user_opt == 1:
+
+        user_opt = None
+
+        print('There are three links to choose from : ')
+        for index in (0,1,2):
+            print(f'{index + 1} - {LINKS[index]}')
+
+        while True:
+            try:
+                user_opt = int(input("what u wanna do ? "))
+                if user_opt <= 0 or user_opt > 3:
+                    continue
+                break
+            except ValueError:
+                print('Give me an int...')
+
+        
+        print(LINKS[user_opt - 1])
 
 
+
+
+
+#    driver = functions.open_driver(DRIVER_PATH)    
 
 
     # closing the driver
-    driver.close()
+#    driver.close()
